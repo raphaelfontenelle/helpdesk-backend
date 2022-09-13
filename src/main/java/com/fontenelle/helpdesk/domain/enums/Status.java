bf59d70adfb06f731498f939a3c35f14bb2623a9
raphaelfontenelle <raphaelfontenelle@MacBook-Pro-de-Raphael.local>
2022-09-13 12:55:49 -0300

    ˜Tipos Enumerados criados

---
+++ src/main/java/com/fontenelle/helpdesk/domain/enums/Status.java
@@ -0,0 +1,35 @@
+package com.fontenelle.helpdesk.domain.enums;
+
+public enum Status {
+
+		ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
+	
+private Status(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+private Integer codigo;
+private String descricao;
+
+public Integer getCodigo() {
+	return codigo;
+}
+public String getDescricao() {
+	return descricao;
+}
+
+public static Status toEnum(Integer cod) {
+	if (cod == null) {
+		return null;
+	}
+	for
+	
+	(Status x : Status.values()) {
+		if (cod.equals(x.getCodigo())) {
+			return x;
+		}
+	}
+	throw new IllegalArgumentException("Perfil Invalido");
+}
+}
