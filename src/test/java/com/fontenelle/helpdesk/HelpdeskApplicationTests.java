06da935703fcb2f8c37a480b5826b349da361ee8
= <raphaelfontenelle@MacBook-Pro-de-Raphael.local>
2022-09-13 09:38:39 -0300

    Projeto iniciado

---
+++ src/test/java/com/fontenelle/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontenelle.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
