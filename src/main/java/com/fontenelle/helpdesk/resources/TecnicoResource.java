a161e38e4190eb7093cd2009f836c92691e6a896
Raphael Fontenelle <raphael.fontenelle@radixeng.com.br>
2022-09-17 12:25:52 -0300

    Padrao DTO para tecnico

--- src/main/java/com/fontenelle/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/fontenelle/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fontenelle.helpdesk.domain.Tecnico;
+import com.fontenelle.helpdesk.domain.dtos.TecnicoDTO;
 import com.fontenelle.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
-		Tecnico obj = this.service.findById(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
 }
